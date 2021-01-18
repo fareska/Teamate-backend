0ae08fe7b64a50a7c1ca01d117e006653d6d4500
Amro97 <57412900+Amro97@users.noreply.github.com>
2021-01-18 19:06:54 +0200

    backend updates+sql

--- server.js
+++ server.js
@@ -1,37 +1,37 @@
 const express = require('express')
+const dotenv = require('dotenv').config()
 const app = express()
 
 const api = require('./server/routes/api')
-
 const Sequelize = require('sequelize')
 
-//const sequelize = new Sequelize('mysql://root:@localhost/sql_testing')
+const sequelize = new Sequelize(process.env.SQL_DB_URI)
 
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-    
+
     next()
 })
 
-//sequelize
-    // .authenticate()
-    // .then(() => {
-    //     console.log('Connection has been established successfully.');
-    // })
-    // .catch(err => {
-    //     console.error('Unable to connect to the database:', err);
-    // })
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    })
 
 
 
 app.use('/', api)
 
-const PORT = 3200
-app.listen(PORT, ()=>{
-    `server running on port ${PORT}`
+const port = process.env.PORT || 3200
+app.listen(port, () => {
+    `server running on port ${port}`
 })
--- server/sql/insertData.js
+++ server/sql/insertData.js
@@ -1,60 +1,68 @@
-const Sequelize = require('sequelize')
-const sequelize = new Sequelize('mysql://root:@localhost/sql_teamate')
-
-const data = require('../../citiesData.json')
-
-// {
-//     "country": "Indonesia",
-//     "geonameid": 1651112,
-//     "name": "Astanajapura",
-//     "subcountry": "West Java"
-//     },
-
-let countries = data.map(d => d.country)
-// country = country.filter((elem, index, self) => index === self.indexOf(elem))
-
-let city = data.map(d => d.city)
-// city = city.filter((elem, index, self) => index === self.indexOf(elem))
-
-const addValue = async function (table, name, type) {
-    let check = await findByID(table, name, type)
-    if(check === 'undefined'){
-        let query =`INSERT INTO ${table} VALUES (null, '${type}')`
-        let result = await sequelize.query(query)
-        return result
-    }
-}
-const findByID = async (table, name, value) => {
-    let query = `SELECT id FROM ${table} WHERE ${name} = "${value}"`
-    let results = await sequelize.query(query)
-    if(!results[0][0]){      
-        return 'undefined' 
-    }
-    return 'ok'
-}
-
-const addUser = async (client) => {
-    // let emailType = client.emailType !== null ? await findByID('email_type', 'email_type', client.emailType) : null
-    // let owner = await findByID('owner', 'owner', client.owner)
-    // let country = await findByID('country', 'country', client.country)
-    // let date = new Date (client.firstContact).toLocaleDateString()
-    // let nameSplit = client.name.split(' ')
-
-    // let query =`INSERT INTO client
-    // VALUES (null, '${nameSplit[1]}', '${nameSplit[0]}', '${client.email}', ${client.sold}, '${date}', ${emailType}, ${owner}, ${country})`
-    // let result = await sequelize.query(query)
-    // return result[0]
-}
-
-// countries.forEach(c => addValue('koko', 'country', c))
-// countries.forEach(c => console.log(c))
-// countries[0]
-// addValue('country', countries[0])
+// const Sequelize = require('sequelize')
+// // const sequelize = new Sequelize('mysql://root:@localhost/sql_teamate')
 
 
-// console.log(countries);
-// emailType.forEach(e => addValue('email_type', e))
-// countries.forEach(c => addValue(console.log(c)))
-// owner.forEach(o => addValue('owner', o))
-// data.forEach(d => addClient(d))
 
+// const data = require('../../citiesData.json')
+
+// // {
+// //     "country": "Indonesia",
+// //     "geonameid": 1651112,
+// //     "name": "Astanajapura",
+// //     "subcountry": "West Java"
+// //     },
+
+// let countries = data.map(d => d.country)
+// // country = country.filter((elem, index, self) => index === self.indexOf(elem))
+
+// let city = data.map(d => d.city)
+// // city = city.filter((elem, index, self) => index === self.indexOf(elem))
+
+// const addValue = async function (table, name, type) {
+//     let check = await findByID(table, name, type)
+//     if(check === 'undefined'){
+//         let query =`INSERT INTO ${table} VALUES (null, '${type}')`
+//         let result = await sequelize.query(query)
+//         return result
+//     }
+// }
+// const findByID = async (table, name, value) => {
+//     let query = `SELECT id FROM ${table} WHERE ${name} = "${value}"`
+//     let results = await sequelize.query(query)
+//     if(!results[0][0]){      
+//         return 'undefined' 
+//     }
+//     return 'ok'
+// }
+
+// const addUser = async (client) => {
+//     // let emailType = client.emailType !== null ? await findByID('email_type', 'email_type', client.emailType) : null
+//     // let owner = await findByID('owner', 'owner', client.owner)
+//     // let country = await findByID('country', 'country', client.country)
+//     // let date = new Date (client.firstContact).toLocaleDateString()
+//     // let nameSplit = client.name.split(' ')
+
+//     // let query =`INSERT INTO client
+//     // VALUES (null, '${nameSplit[1]}', '${nameSplit[0]}', '${client.email}', ${client.sold}, '${date}', ${emailType}, ${owner}, ${country})`
+//     // let result = await sequelize.query(query)
+//     // return result[0]
+// }
+
+// // countries.forEach(c => addValue('koko', 'country', c))
+// // countries.forEach(c => console.log(c))
+// // countries[0]
+// // addValue('country', countries[0])
+
+
+// // console.log(countries);
+// // emailType.forEach(e => addValue('email_type', e))
+// // countries.forEach(c => addValue(console.log(c)))
+// // owner.forEach(o => addValue('owner', o))
+// // data.forEach(d => addClient(d))
+
+// // const Sequelize = require('sequelize')
+
+// // const sequelize = new Sequelize(process.env.SQL_DB_URI)
+
+// // sequelize.query(`
+// // SELECT * FROM frequency`).then((res)=>console.log(res))
