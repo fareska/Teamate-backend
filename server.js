bedcebbd14ad60079aa69adf292ebc0e009d160d
fareska <fares.xb@gmail.com>
2021-01-21 12:25:14 +0200

    herokuBug

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ const sequelize = new Sequelize(process.env.SQL_DB_URI)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
 
-    next()
-})
+//     next()
+// })
 
 sequelize
     .authenticate()
